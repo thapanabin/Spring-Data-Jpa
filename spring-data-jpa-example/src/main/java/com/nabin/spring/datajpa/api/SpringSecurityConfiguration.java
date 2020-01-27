e3f6ba254b67c352fb0d4bd9e5e5643ffe1d1a5c
thapanabin <thapanabincr7@outlook.com>
2020-01-26 23:03:08 -0600

    configured spring security authorization

--- spring-data-jpa-example/src/main/java/com/nabin/spring/datajpa/api/SpringSecurityConfiguration.java
+++ spring-data-jpa-example/src/main/java/com/nabin/spring/datajpa/api/SpringSecurityConfiguration.java
@@ -2,6 +2,7 @@ package com.nabin.spring.datajpa.api;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -26,6 +27,15 @@ public class SpringSecurityConfiguration extends WebSecurityConfigurerAdapter{
 	@Bean
 	public PasswordEncoder getPasswordEncoder() {
 		return NoOpPasswordEncoder.getInstance();//No ops password encoder will do nothing just doing this because spring security only deal with the hashed password (encrypted password) so it will look for the encoder for testing purpose we are not using the hashed password
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+		.antMatchers("/admin").hasRole("ADMIN")//need to put most restrictive at the top
+		.antMatchers("/user").hasAnyRole("USER","ADMIN")
+		.antMatchers("/").permitAll()
+		.and().formLogin();
 	}
 
 }
