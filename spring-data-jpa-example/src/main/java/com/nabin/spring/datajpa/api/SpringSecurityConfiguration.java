69de477c208114a4631ba8a37fe3fd487f092bca
thapanabin <thapanabincr7@outlook.com>
2020-01-26 21:53:40 -0600

    configured spring security authentication

---
+++ spring-data-jpa-example/src/main/java/com/nabin/spring/datajpa/api/SpringSecurityConfiguration.java
@@ -0,0 +1,31 @@
+package com.nabin.spring.datajpa.api;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity //tell spring security that this is web security configuration
+public class SpringSecurityConfiguration extends WebSecurityConfigurerAdapter{
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		//set your configuration in the auth object
+		auth.inMemoryAuthentication()
+			.withUser("nabin")
+			.password("nabin")
+			.roles("USER")
+			.and()
+			.withUser("puspa")
+			.password("puspa")
+			.roles("ADMIN");
+	}
+	
+	@Bean
+	public PasswordEncoder getPasswordEncoder() {
+		return NoOpPasswordEncoder.getInstance();//No ops password encoder will do nothing just doing this because spring security only deal with the hashed password (encrypted password) so it will look for the encoder for testing purpose we are not using the hashed password
+	}
+
+}
