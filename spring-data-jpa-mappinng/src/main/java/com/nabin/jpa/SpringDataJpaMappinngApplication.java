f32e084281e341fb75dd109f9c939a7c560e7b2c
thapanabin <thapanabincr7@outlook.com>
2020-04-17 17:06:31 -0500

    implemented open-api documentation for microservice

--- spring-data-jpa-mappinng/src/main/java/com/nabin/jpa/SpringDataJpaMappinngApplication.java
+++ spring-data-jpa-mappinng/src/main/java/com/nabin/jpa/SpringDataJpaMappinngApplication.java
@@ -3,7 +3,11 @@ package com.nabin.jpa;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
+@SpringBootApplication//openapi doc
+@OpenAPIDefinition(info = @Info(title = "Product API",version = "2.1.2",description = "Product Microservices"))
 public class SpringDataJpaMappinngApplication {
 
 	public static void main(String[] args) {
